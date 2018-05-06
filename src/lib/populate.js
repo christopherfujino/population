0b9953ccdc238718141f706ad424a711f1e9777b
Christopher Fujino <christopherfujino@gmail.com>
2018-05-05 22:18:16 -0700

    currying?

--- src/lib/populate.js
+++ src/lib/populate.js
@@ -1,5 +1,7 @@
 const validateCount = count => count >= 1 && Math.floor(count) === count;
 
+const generateUniqueName = existingHash => seedName => seedName.length > 0 ?  : generateUniqueName(generateRandomName())
+
 const populate = count => {
   if (!validateCount(count)) return {};
   const population = {};
